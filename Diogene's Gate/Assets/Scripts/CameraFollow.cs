7c5c32b8e3926ed156a552e763a64dd10f25e672
Joshua Mateer <jcm155@pitt.edu>
2022-04-08 20:11:46 -0400

    adding animations

---
+++ Diogene's Gate/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform playerTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void LateUpdate() {
+        Vector3 temp = transform.position;
+        temp.x = playerTransform.position.x;
+        temp.y = playerTransform.position.y;
+        transform.position = temp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Diogene's Gate/Assets/Scripts/PlayerController.cs
+++ Diogene's Gate/Assets/Scripts/PlayerController.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+    public int facing;
     public float playerSpeed;
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        facing = 0;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -18,9 +21,17 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f) {
             transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * playerSpeed * Time.deltaTime, 0f, 0f));
+            facing = Input.GetAxisRaw("Horizontal") > 0.5f ? 1 : 3;
         }
         if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f) {
             transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * playerSpeed * Time.deltaTime, 0f));
+            facing = Input.GetAxisRaw("Vertical") > 0.5f ? 2 : 0;
         }
+        animator.SetInteger("Facing", facing);
+        Quaternion temp = transform.rotation;
+        temp.x = 0;
+        temp.y = 0;
+        temp.z = 0;
+        transform.rotation = temp;
     }
 }
