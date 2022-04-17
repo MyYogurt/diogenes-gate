328666b6139c5952102e1219fda3257ada9af59f
jcm155 <56082432+jcm155@users.noreply.github.com>
2022-04-16 22:04:40 -0400

    updating assets

---
+++ Diogene's Gate/Assets/Scripts/BattleController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleController : MonoBehaviour
+{
+    public bool inBattleScene;
+    public List<GameObject> players;
+    public List<GameObject> enemies;
+    Animator animator;
+    int enemyType;
+    int turn;
+    int subturn;
+    List<string> charNames;
+
+
+    void startBattle()
+    {
+        turn = 0;
+        subTurn = 0;
+        enemyType = Random.Range(0, 3);
+        animator.SetInteger("enemyType", enemyType);
+        inBattleScene = true;
+    }
+
+    void endBattle()
+    {
+        inBattleScene = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("o"))
+        {
+            startBattle();
+            Debug.Log("now starting battle...\n");
+        }
+        if (Input.GetKeyDown("p"))
+        {
+            endBattle();
+            Debug.Log("now ending battle...");
+        }
+        if (Random.Range((float)0.0, (float)1.0) < 0.001 && !inBattleScene)
+        {
+            startBattle();
+        }
+        if (inBattleScene)
+        {
+            GetComponent<SpriteRenderer>().enabled = true;
+        } else {
+            GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+}
